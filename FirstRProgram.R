3403cc9d6e23c41b7ee33b0d1189c1c254948177
Anmay Gupta <anmaygupta1@gmail.com>
2021-05-25 18:31:27 -0500

    Did the tutorial

---
+++ FirstRProgram.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "My First R Program"
+#' author: "Anmay Gupta"
+#' date: "5/25/2021"
+#' output: 
+#'   html_document:
+#'     keep_md: true
+#' ---
+
+#+ r test, echo = FALSE
+
+## insert your WORKING code here
+print ("Hello, World!")
+myString <- "Not hello World"
+print (myString)
